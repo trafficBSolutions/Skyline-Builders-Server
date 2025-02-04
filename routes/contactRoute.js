d36d92e1460242425da770d8d99799cc74b7270f
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2025-02-04 15:34:13 -0500

    Update contactRoute.js

--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -7,7 +7,7 @@ const { submitContact } = require('../controls/contactControl');
 router.use(
     cors({
         credentials: true,
-        origin: 'http://localhost:5173'
+        origin: 'http://eloquent-lamington-957e5e.netlify.app'
     })
 );
 
